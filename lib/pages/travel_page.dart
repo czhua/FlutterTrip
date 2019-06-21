f77c6bab70047756723b27c8906631b9a9de629f
chan <zehua.ch@gmail.com>
2019-06-21 09:29:39 +0800

    first commit

---
+++ lib/pages/travel_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TravelPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _TravelPageState();
+  }
+}
+
+class _TravelPageState extends State<TravelPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('旅拍'),
+      ),
+    );
+  }
+}
